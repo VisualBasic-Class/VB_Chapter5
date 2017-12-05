42b00e70b8bd970ccad2bd012341be407540e873
Chane Eckhardt <33033313+ceckhardt22@users.noreply.github.com>
2017-12-05 11:02:27 -0600

    update_chapter

---
+++ CoinToss/CoinToss/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picCoinTails = New System.Windows.Forms.Label()
+        Me.picCoinHeads = New System.Windows.Forms.Label()
+        Me.btnTossCoin = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'picCoinTails
+        '
+        Me.picCoinTails.Image = Global.WindowsApplication1.My.Resources.Resources.coin_21
+        Me.picCoinTails.Location = New System.Drawing.Point(181, 67)
+        Me.picCoinTails.Name = "picCoinTails"
+        Me.picCoinTails.Size = New System.Drawing.Size(136, 128)
+        Me.picCoinTails.TabIndex = 1
+        '
+        'picCoinHeads
+        '
+        Me.picCoinHeads.Image = Global.WindowsApplication1.My.Resources.Resources.coin1
+        Me.picCoinHeads.Location = New System.Drawing.Point(28, 73)
+        Me.picCoinHeads.Name = "picCoinHeads"
+        Me.picCoinHeads.Size = New System.Drawing.Size(121, 112)
+        Me.picCoinHeads.TabIndex = 0
+        '
+        'btnTossCoin
+        '
+        Me.btnTossCoin.Location = New System.Drawing.Point(24, 223)
+        Me.btnTossCoin.Name = "btnTossCoin"
+        Me.btnTossCoin.Size = New System.Drawing.Size(125, 23)
+        Me.btnTossCoin.TabIndex = 2
+        Me.btnTossCoin.Text = "Toss the Coin"
+        Me.btnTossCoin.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(202, 223)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(125, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(349, 267)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnTossCoin)
+        Me.Controls.Add(Me.picCoinTails)
+        Me.Controls.Add(Me.picCoinHeads)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents picCoinHeads As System.Windows.Forms.Label
+    Friend WithEvents picCoinTails As System.Windows.Forms.Label
+    Friend WithEvents btnTossCoin As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ CoinToss/CoinToss/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnTossCoin_Click(sender As Object, e As EventArgs) Handles btnTossCoin.Click
+        Dim intSideUp As Integer
+        Dim rand As New Random
+
+        ' Get a random number in the range of 0 to 1.
+        ' 0 means tails up; 1 means heads up.
+        intSideUp = rand.Next(2)
+
+        ' Display the side that is up.
+        If intSideUp = 0 Then
+            picCoinHeads.Visible = True
+            picCoinTails.Visible = False
+        Else
+            picCoinHeads.Visible = False
+            picCoinHeads.Visible = True
+        End If
+    End Sub
+End Class
