f0e76c4de345d736c42c8863b686d2eb7656d4f3
Chane Eckhardt <33033313+ceckhardt22@users.noreply.github.com>
2017-12-05 10:46:25 -0600

    update_chapter

---
+++ RightTriangle/RightTriangle/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Sub calcSideC(ByVal Aside As Double, ByVal Bside As Double)
+        Dim Cside As Double
+        Cside = Math.Sqrt(Aside * Aside + Bside * Bside)
+        txtSideC.Text = Cside.ToString
+    End Sub
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Dim result As Double
+        If radioC.Checked = True Then
+            calcSideC(CDbl(txtSideA.Text), CDbl(txtSideB.Text))
+        End If
+    End Sub
+End Class
