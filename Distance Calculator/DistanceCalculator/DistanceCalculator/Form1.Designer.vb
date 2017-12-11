faf78172c10d83132517e4327807d71dc8e16762
Chane Eckhardt <33033313+ceckhardt22@users.noreply.github.com>
2017-12-11 11:07:47 -0600

    update_chapter

---
+++ Distance Calculator/DistanceCalculator/DistanceCalculator/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DistanceCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblSpeed = New System.Windows.Forms.Label()
+        Me.lblHours = New System.Windows.Forms.Label()
+        Me.txtSpeed = New System.Windows.Forms.TextBox()
+        Me.txtHours = New System.Windows.Forms.TextBox()
+        Me.listResults = New System.Windows.Forms.ListBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblSpeed
+        '
+        Me.lblSpeed.AutoSize = True
+        Me.lblSpeed.Location = New System.Drawing.Point(21, 59)
+        Me.lblSpeed.Name = "lblSpeed"
+        Me.lblSpeed.Size = New System.Drawing.Size(38, 13)
+        Me.lblSpeed.TabIndex = 0
+        Me.lblSpeed.Text = "Speed"
+        '
+        'lblHours
+        '
+        Me.lblHours.AutoSize = True
+        Me.lblHours.Location = New System.Drawing.Point(201, 59)
+        Me.lblHours.Name = "lblHours"
+        Me.lblHours.Size = New System.Drawing.Size(35, 13)
+        Me.lblHours.TabIndex = 1
+        Me.lblHours.Text = "Hours"
+        '
+        'txtSpeed
+        '
+        Me.txtSpeed.Location = New System.Drawing.Point(65, 56)
+        Me.txtSpeed.Name = "txtSpeed"
+        Me.txtSpeed.Size = New System.Drawing.Size(112, 20)
+        Me.txtSpeed.TabIndex = 2
+        '
+        'txtHours
+        '
+        Me.txtHours.Location = New System.Drawing.Point(242, 56)
+        Me.txtHours.Name = "txtHours"
+        Me.txtHours.Size = New System.Drawing.Size(112, 20)
+        Me.txtHours.TabIndex = 3
+        '
+        'listResults
+        '
+        Me.listResults.FormattingEnabled = True
+        Me.listResults.Location = New System.Drawing.Point(74, 111)
+        Me.listResults.Name = "listResults"
+        Me.listResults.Size = New System.Drawing.Size(256, 186)
+        Me.listResults.TabIndex = 4
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(40, 324)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 5
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(290, 324)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(163, 324)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'DistanceCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(401, 368)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.listResults)
+        Me.Controls.Add(Me.txtHours)
+        Me.Controls.Add(Me.txtSpeed)
+        Me.Controls.Add(Me.lblHours)
+        Me.Controls.Add(Me.lblSpeed)
+        Me.Name = "DistanceCalc"
+        Me.Text = "Distance Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblSpeed As System.Windows.Forms.Label
+    Friend WithEvents lblHours As System.Windows.Forms.Label
+    Friend WithEvents txtSpeed As System.Windows.Forms.TextBox
+    Friend WithEvents txtHours As System.Windows.Forms.TextBox
+    Friend WithEvents listResults As System.Windows.Forms.ListBox
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ Distance Calculator/DistanceCalculator/DistanceCalculator/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class DistanceCalc
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim dblDistanceTraveled As Double
+        Dim dblAnswer = 0
+
+        Do While dblAnswer < CDbl(txtHours.Text)
+            dblAnswer = dblAnswer + 1
+            dblDistanceTraveled = (txtSpeed.Text) * dblAnswer
+            listResults.Items.Add(dblAnswer.ToString + "       " + dblDistanceTraveled.ToString)
+        Loop
+        listResults.Items.Add("Total Distance: " + dblDistanceTraveled.ToString)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtHours.Clear()
+        txtSpeed.Clear()
+        listResults.Items.Clear()
+    End Sub
+End Class
